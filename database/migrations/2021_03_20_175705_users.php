6b10193c20153479c7cc5f973dc0adcc4cc0e8b9
nivcoo <nico64800@live.fr>
2021-03-22 13:10:34 +0100

    feat. add promotions and centers

--- database/migrations/2021_03_20_175705_users.php
+++ database/migrations/2021_03_20_175705_users.php
@@ -15,7 +15,6 @@ class Users extends Migration
     {
         if (!Schema::hasTable('users')) {
             Schema::create('users', function (Blueprint $table) {
-
                 $table->increments('id');
                 $table->string('first_name');
                 $table->string('last_name');
