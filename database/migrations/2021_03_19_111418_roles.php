bf8cf5753250508c2c52e07605600e30371dc752
nivcoo <nico64800@live.fr>
2021-03-27 11:40:19 +0100

    improv. remove useless default permissions

--- database/migrations/2021_03_19_111418_roles.php
+++ database/migrations/2021_03_19_111418_roles.php
@@ -98,19 +98,10 @@ class Roles extends Migration
             DB::table('permissions_roles')->insert([
                     ['role_id' => 1, 'permission_id' => 1],
                     ['role_id' => 1, 'permission_id' => 2],
-                    ['role_id' => 1, 'permission_id' => 3],
-                    ['role_id' => 1, 'permission_id' => 4],
-                    ['role_id' => 1, 'permission_id' => 5],
                     ['role_id' => 2, 'permission_id' => 1],
                     ['role_id' => 2, 'permission_id' => 2],
-                    ['role_id' => 2, 'permission_id' => 3],
-                    ['role_id' => 2, 'permission_id' => 4],
-                    ['role_id' => 2, 'permission_id' => 5],
                     ['role_id' => 3, 'permission_id' => 1],
                     ['role_id' => 3, 'permission_id' => 2],
-                    ['role_id' => 3, 'permission_id' => 3],
-                    ['role_id' => 3, 'permission_id' => 4],
-                    ['role_id' => 3, 'permission_id' => 5],
 
                 ]
             );
