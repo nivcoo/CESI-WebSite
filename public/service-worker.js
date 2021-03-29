42ba3f17e9b5ea2172e113c94cdcc838abfe2924
Wati-tag <72252391+Wati-tag@users.noreply.github.com>
2021-03-29 10:47:52 +0200

    Add files via upload

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,26 @@
+const cacheName = "cache-v1";
+const assets = [
+  "/",
+  "/panel",
+];
+
+// mettre en cache
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// fetcher le cache
+self.addEventListener("fetch", (e) => {
+  console.log(e.request);
+  e.respondWith(
+    caches.match(e.request).then((cache) => {
+      return cache || fetch(e.request);
+    })
+  );
+});
+
 importScripts('sw-toolbox.js');
